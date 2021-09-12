03f867c67f9542b9945c837237149cae95fb8334
beld <belfami@gmail.com>
2021-09-12 14:35:34 +0200

    sort script now does not update ToC

--- sort.py
+++ sort.py
@@ -20,18 +20,9 @@ def sort_blocks():
         read_me = read_me_file.read()
 
     # Separating the 'table of contents' from the contents (blocks)
-    table_of_contents = ''.join(read_me.split('- - -')[0])
-    blocks = ''.join(read_me.split('- - -'))
-    print(blocks)
-    print("--------------")
-    print("--------------")
-    print("--------------")
-    print("--------------")
-    print("--------------")
-    try:
-        blocks = ''.join(read_me.split('- - -')[1]).split('\n# ')
-    except:
-        pass
+    table_of_contents = ''.join(read_me.split('---')[0])
+    blocks = ''.join(read_me.split('---')[1]).split('\n# ')
+    # print(table_of_contents)
     for i in range(len(blocks)):
         if i == 0:
             blocks[i] = blocks[i] + '\n'
@@ -39,18 +30,20 @@ def sort_blocks():
             blocks[i] = '# ' + blocks[i] + '\n'
 
     # Sorting the libraries
-    inner_blocks = sorted(blocks[0].split('##'))
+    # inner_blocks = sorted(blocks[0].split('##')) ## this sorts categories as well which we do not want
+    inner_blocks = blocks[0].split('##')
     for i in range(1, len(inner_blocks)):
         if inner_blocks[i][0] != '#':
             inner_blocks[i] = '##' + inner_blocks[i]
     inner_blocks = ''.join(inner_blocks)
-
     # Replacing the non-sorted libraries by the sorted ones and gathering all at the final_README file
     blocks[0] = inner_blocks
-    final_README = table_of_contents + '- - -' + ''.join(blocks)
+    final_README = table_of_contents + '---\n\n' + ''.join(blocks).strip() + '\n'
+    print(final_README)
 
     with open('README.md', 'w+') as sorted_file:
         sorted_file.write(final_README)
+
 
 def main():
     # First, we load the current README into memory as an array of lines
@@ -77,10 +70,21 @@ def main():
             blocks.append([line])
             last_indent = None
 
+    ignore_sort_list = ["- [Awesome Utrecht University](#awesome-utrecht-university)",
+    "- [About](#about)",
+    "- [Research projects with code](#research-projects-with-code)",
+    "- [Research software](#research-software)",
+    "- [Research Data](#research-data)",
+    "- [Research project management](#research-project-management)",
+    "- [Education and workshops](#education-and-workshops)",
+    "- [Collaboration Groups](#collaboration-groups)",
+    "- [Contact](#contact)",
+    "- [Contributing](#contributing)"    ]
+    # print(not any(substring in ''.join(blocks[20]) for substring in ignore_sort_list))
     with open('README.md', 'w+') as sorted_file:
         # Then all of the blocks are sorted individually
         blocks = [
-            ''.join(sorted(block, key=str.lower)) for block in blocks
+            ''.join(sorted(block, key=str.lower)) if not any(toc_block in ''.join(block) for toc_block in ignore_sort_list) else ''.join(block) for block in blocks # do not sort ToC
         ]
         # And the result is written back to README.md
         sorted_file.write(''.join(blocks))
